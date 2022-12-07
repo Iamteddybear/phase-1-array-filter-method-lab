e9931c789b828c3cc2d1caae4294230afa52d223
ned <nedschultz@outlook.com>
2022-12-07 14:37:45 +1000

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+function findMatching(driverNames, nameToMatch) {
+    return driverNames.filter(
+        (driverName) => driverName.toLowerCase() === nameToMatch.toLowerCase()
+    );
+}
+
+function fuzzyMatch(driverNames, startingCharacters) {
+    let fuzzyMatches = driverNames.filter((driverName) =>
+        driverName.startsWith(startingCharacters)
+    );
+
+    return fuzzyMatches;
+}
+
+function matchName(driverObjects, nameToMatch) {
+    return driverObjects.filter(
+        (driverObject) => driverObject.name === nameToMatch
+    );
+}
